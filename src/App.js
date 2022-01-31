2df576f4698e6c72e820499b0bd77c77484430df
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2022-01-31 15:54:58 -0800

    pages routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/layout/Navbar.jsx'
-
+import Footer from "./components/layout/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import About from "./pages/About.jsx";
 
 function App() {
   return (
@@ -8,8 +11,21 @@ function App() {
    <div className='flex flex-col justify-between h-screen'>
    <Navbar/>
 
-    <main>Content</main>
 
+    <main className="conatiner mx-auto px-3 pb-12">
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/notfound' element={<NotFound/>}/>
+    <Route path='/*' element={<NotFound/>}/>
+
+
+    </Routes>
+
+
+    </main>
+
+<Footer/>
     </div>
  
     </Router>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+export default function NotFound() {
+  return (
+    <div className='hero'>
+      <div className="text-center hero-content">
+          not found
+      </div>
+    </div>
+  )
+}
